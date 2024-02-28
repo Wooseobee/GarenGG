10aa2ad956ba27168741161b69473e186036ac76
유광우 <godsun7892@naver.com>
2024-02-28 21:59:03 +0900

    feat : mainpage

--- front/Garen_Front/src/App.vue
+++ front/Garen_Front/src/App.vue
@@ -1,50 +1,57 @@
 <template>
-  <div class="background_gif">
-    <div class="background_overlay"></div>
-    <div class="main">
-      <Header/>
-      <RouterView/>
-    </div>
+  <div>
+
+    <body class="index-page" data-bs-spy="scroll" data-bs-target="#navmenu">
+      <Header />
+      <RouterView />
+    </body>
+
   </div>
 </template>
 
 <script setup>
-import Header from "@/components/common/Header.vue";
+import { RouterView } from 'vue-router';
+import Header from "@/components/common/Header.vue"
+
 </script>
 
-<style scoped>
-.background_gif {
-  /* 전체 페이지를 커버하도록 설정 */
-  width: 100vw;
-  height: 100vh;
-  /* 배경 이미지를 담을 자식 요소에 대한 위치 설정 */
-  position: relative;
+<style>
+:root {
+  --default-font: "Open Sans", system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  --heading-font: "Montserrat", sans-serif;
+  --nav-font: "Lato", sans-serif;
 }
 
-.background_overlay {
-  /* 배경 이미지로 PNG 파일 지정 */
-  background-image: url('@/assets/garen-background.gif');
-  /* 배경 이미지가 컨테이너에 맞게 조정되도록 설정 */
-  background-size: cover;
-  /* 배경 이미지가 페이지 스크롤에 따라 움직이지 않도록 설정 */
-  background-attachment: fixed;
-  /* 전체 요소를 커버하도록 설정 */
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  /* 배경 이미지에만 투명도 적용 */
-  opacity: 0.6;
-  /* z-index를 사용하여 배경을 내용 뒤로 보냄 */
-  z-index: -1;
+/* Global Colors */
+:root {
+  --background-color: #ffffff;
+  --background-color-rgb: 255, 255, 255;
+  --default-color: #212529;
+  --default-color-rgb: 33, 37, 41;
+  --accent-color: #007BFF;
+  --accent-color-rgb: 232, 69, 69;
+  --heading-color: #32353a;
+  --heading-color-rgb: 50, 53, 58;
+  --contrast-color: #ffffff;
+  --contrast-color-rgb: 255, 255, 255;
 }
 
-.main {
-  /* 내용에는 투명도 적용하지 않음 */
-  opacity: 1;
-  /* 필요한 경우, z-index를 조정하여 내용을 더 부각시킴 */
-  position: relative;
-  z-index: 1;
+/* Nav Menu Colors */
+:root {
+  --nav-color: #3a3939;
+  --nav-hover-color: #e84545;
+  --nav-dropdown-color: #3a3939;
+  --nav-dropdown-hover-color: #e84545;
+  --nav-dropdown-background-color: #ffffff;
 }
+
+/* Smooth scroll */
+:root {
+  scroll-behavior: smooth;
+}
+
+a, .router-link-exact-active, .router-link-active {
+  text-decoration: none;
+}
+
 </style>
--- front/Garen_Front/src/views/HomeView.vue
+++ front/Garen_Front/src/views/HomeView.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    HI
+    <section id="hero" class="hero">
+
+      <img src="@/assets/garen-background.gif" alt="" data-aos="fade-in">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-10">
+            <h2 data-aos="fade-up" data-aos-delay="100">가렌.GG</h2>
+            <p data-aos="fade-up" data-aos-delay="200">나에게 딱 맞는 새로운 주챔을 추천 받으세요</p>
+          </div>
+          <div class="col-lg-5">
+            <div class="sign-up-form d-flex" data-aos="fade-up" data-aos-delay="300">
+              <input type="text" class="form-control" placeholder="플레이어 이름#KR1">
+              <input type="submit" class="btn btn-primary" value="검색">
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
   </div>
 </template>
 
@@ -10,4 +29,96 @@
 
 <style scoped>
 
+.hero {
+  --default-color: #ffffff;
+  --default-color-rgb: 255, 255, 255;
+  --background-color: #000000;
+  --background-color-rgb: 0, 0, 0;
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
+  padding: 160px 0 80px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.hero img {
+  position: absolute;
+  inset: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  z-index: 1;
+}
+
+.hero:before {
+  content: "";
+  background: rgba(var(--background-color-rgb), 0.5);
+  position: absolute;
+  inset: 0;
+  z-index: 2;
+}
+
+.hero .container {
+  position: relative;
+  margin-left: 150px;
+  z-index: 3;
+}
+
+.hero h2 {
+  color: white;
+  margin: 0;
+  font-size: 58px;
+  font-weight: 700;
+}
+
+.hero p {
+  color: white;
+  margin: 5px 0 0 0;
+  font-size: 25px;
+}
+
+.hero .sign-up-form {
+  width: 120%;
+  height: 90%;
+  margin-top: 20px;
+  padding: 10px;
+  border-radius: 7px;
+  background: #fff;
+  box-shadow: 0 15px 30px 0 rgba(0, 0, 0, 0.1);
+}
+
+.hero .sign-up-form .form-control {
+  height: 100%;
+  border: none;
+}
+
+.hero .sign-up-form .form-control:active,
+.hero .sign-up-form .form-control:focus {
+  outline: none;
+  box-shadow: none;
+}
+
+.hero .sign-up-form input[type=submit] {
+  box-shadow: 0 15px 30px 0 rgba(0, 0, 0, 0.2);
+  background-color: var(#87CEEB);
+  border-color: var(#87CEEB);
+  padding: 8px 20px 10px 20px;
+  border-radius: 7px;
+  color: var(--contrast-color);
+}
+
+
+@media (max-width: 768px) {
+  .hero h2 {
+    font-size: 32px;
+  }
+
+  .hero p {
+    font-size: 18px;
+  }
+}
 </style>
