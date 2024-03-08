95018bb86fe95c142f9ed2f9c9a0ffb899c82388
imdeokey <imdeokey@gmail.com>
2024-03-08 12:45:54 +0900

    Fix: 일단 추천 되게 수정

--- AI Train/app/recommend.py
+++ AI Train/app/recommend.py
@@ -7,7 +7,7 @@ from train import load_data
 
 
 # 추천 함수
-def recommend_champs(df_svd_preds, user_id, ori_champs_df, ori_scores_df, num_recommendations=5):
+def recommend_champs(df_svd_preds, user_id, champ_data, score_data, num_recommendations=5):
     
     # index와 user_id를 맞추는 부분. 현재는 index와 user_id 둘 다 0부터 시작하므로 변화 x.
     user_row_number = user_id 
@@ -16,16 +16,20 @@ def recommend_champs(df_svd_preds, user_id, ori_champs_df, ori_scores_df, num_re
     sorted_user_predictions = df_svd_preds.iloc[user_row_number].sort_values(ascending=False)
 
     # 원본 평점 데이터에서 user id에 해당하는 데이터를 뽑아낸다. 
-    user_data = ori_scores_df[ori_scores_df.user_id == user_id]
+    user_data = score_data[score_data.user_id == user_id]
     
     # 위에서 뽑은 user_data와 원본 챔피언 데이터를 합친다. 
-    user_history = user_data.merge(ori_champs_df, on = 'id').sort_values(['score'], ascending=False)
-
+    user_history = pd.merge(user_data, champ_data, left_on='champion', right_on='id', how='inner').sort_values(['score'], ascending=False)
+    user_history.drop(columns=['id', 'name'], inplace=True)
+    # user_history = user_data.merge(champ_data, on = 'id')
+    # user_history = pd.merge(user_data, champ_data, left_on = 'champion', right_on='id', how='inner')# .sort_values(['score'], ascending=False)
     # 원본 챔피언 데이터에서 사용자가 본 챔피언 데이터를 제외한 데이터를 추출
-    recommendations = ori_champs_df[~ori_champs_df['id'].isin(user_history['id'])]
+    recommendations = champ_data[~champ_data['id'].isin(user_history['champion'])]
+
 
     # 사용자의 챔피언 평점이 높은 순으로 정렬된 데이터와 위 recommendations을 합친다. 
-    recommendations = recommendations.merge( pd.DataFrame(sorted_user_predictions).reset_index(), on = 'id')
+    recommendations = pd.merge(recommendations, sorted_user_predictions, left_on='id', right_on='champion', how='inner')
+    # recommendations = recommendations.merge( pd.DataFrame(sorted_user_predictions).reset_index(), on = 'id')
     # 컬럼 이름 바꾸고 정렬해서 return
     recommendations = recommendations.rename(columns = {user_row_number: 'Predictions'}).sort_values('Predictions', ascending = False).iloc[:num_recommendations, :]
                       
