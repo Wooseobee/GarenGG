28aa21c50ee66afa8e075b2a0b7d750f91472bc4
wooseobee <dntjq0807@naver.com>
2024-03-11 15:08:36 +0900

    feat: 기능3 매치 승부 예측 구현 - 랜덤 경기 불러오기

---
+++ back/src/main/java/gg/garen/back/matchPrediction/controller/MatchPredictionController.java
@@ -0,0 +1,19 @@
+package gg.garen.back.matchPrediction.controller;
+
+import gg.garen.back.matchPrediction.dto.RandomMatchResponseDto;
+import gg.garen.back.matchPrediction.service.MatchPredictionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MatchPredictionController {
+
+    private final MatchPredictionService matchPredictionService;
+
+    @GetMapping("/getRandomMatch")
+    public RandomMatchResponseDto getRandomMatch() {
+        return matchPredictionService.getRandomMatch();
+    }
+}
