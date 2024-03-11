e9660ed4d503ca830ef8424822c4f7c54fac7e31
imdeokey <imdeokey@gmail.com>
2024-03-11 14:58:46 +0900

    팀 조합 카드 추가

--- front/src/components/team_comp/Team_Comp.vue
+++ front/src/components/team_comp/Team_Comp.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-        팀 조합 페이지 입니다
-    </div>
+  팀 조합 페이지 입니다
+  <div class="cards">
+    <card />
+    <card />
+    <card />
+  </div>
 </template>
 
 <script setup>
-
+import Card from "../common/Card.vue";
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
