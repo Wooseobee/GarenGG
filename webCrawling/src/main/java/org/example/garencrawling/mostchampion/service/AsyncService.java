c23a654e9916ac2a8bfe053cb8df48b6d11e0c05
KimJinyong <wlsdyd4@naver.com>
2024-03-12 23:29:09 +0900

    [BE] Feat : hi
    Former-commit-id: 45cab3f657be017c5b0d7db23cf3e7279a5edd62

--- webCrawling/src/main/java/org/example/garencrawling/mostchampion/service/AsyncService.java
+++ webCrawling/src/main/java/org/example/garencrawling/mostchampion/service/AsyncService.java
@@ -34,7 +34,11 @@ public class AsyncService {
         int endIndex = subFindedPlayerInfos.size() - 1;
 
         while (currentStartIndex <= endIndex) {
+
             int currentEndIndex = Math.min(currentStartIndex + GlobalConstants.saveSize - 1, endIndex);
+
+            System.out.println("현재 시간: " + GlobalConstants.formatter.format(new Date()) + " threadNumber = " + threadNumber+" 일하는 중");
+
             crawling(subFindedPlayerInfos.subList(currentStartIndex, currentEndIndex + 1), threadNumber);
             currentStartIndex = currentEndIndex + 1;
         }
@@ -43,9 +47,10 @@ public class AsyncService {
     }
 
     public void crawling(List<PlayerInfo> playerInfos, int threadNumber) throws InterruptedException {
+
         WebElement element;
         ChromeOptions options;
-        ChromeDriver driver;
+        ChromeDriver driver = null;
         WebDriverWait wait;
         List<WebElement> rows;
         ArrayList<PlayerCurSoloRank> playerCurSoloRanks = new ArrayList<>();
@@ -54,193 +59,199 @@ public class AsyncService {
 
         ////////////////////////////////////////////////////////////////////////////////////////////
 
-        WebDriverManager.chromedriver().setup();
-        options = new ChromeOptions();
+        try {
+            WebDriverManager.chromedriver().setup();
+            options = new ChromeOptions();
 
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--start-maximized");
-        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+            options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--disable-popup-blocking");
+            options.addArguments("--disable-infobars");
+            options.addArguments("--start-maximized");
+            options.setPageLoadStrategy(PageLoadStrategy.NONE);
 
-        driver = new ChromeDriver(options);
+            driver = new ChromeDriver(options);
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(GlobalConstants.waitTime)); // 명시적 대기 시간 설정
+            wait = new WebDriverWait(driver, Duration.ofSeconds(GlobalConstants.waitTime)); // 명시적 대기 시간 설정
 
-        ////////////////////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////////////////
 
-        for (int index = 0; index < playerInfos.size(); index++) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("threadNumber = ").append(threadNumber).append(" ");
+            for (int index = 0; index < playerInfos.size(); index++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("threadNumber = ").append(threadNumber).append(" ");
 
-            PlayerInfo playerInfo = playerInfos.get(index);
-            sb.append("playerInfo.getPlayerId() = ").append(playerInfo.getPlayerId()).append(" ");
-            sb.append("playerInfo.getUserNickname() = ").append(playerInfo.getUserNickname()).append(" ");
+                PlayerInfo playerInfo = playerInfos.get(index);
+                sb.append("playerInfo.getPlayerId() = ").append(playerInfo.getPlayerId()).append(" ");
+                sb.append("playerInfo.getUserNickname() = ").append(playerInfo.getUserNickname()).append(" ");
 
-            PlayerCurSoloRank playerCurSoloRank = new PlayerCurSoloRank();
-            playerCurSoloRank.setPlayerId(playerInfo.getPlayerId());
+                PlayerCurSoloRank playerCurSoloRank = new PlayerCurSoloRank();
+                playerCurSoloRank.setPlayerId(playerInfo.getPlayerId());
 
-            // 메인 접속
-            driver.get("https://fow.kr/find/" + playerInfo.getUserNickname());
+                // 메인 접속
+                driver.get("https://fow.kr/find/" + playerInfo.getUserNickname());
 
-            // 닉네임
-            tryCount = 1;
-            while (tryCount <= GlobalConstants.tryMaxCount) {
-                try {
-                    getText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.topp > div.profile > div:nth-child(2) > span.username"))).getText();
-                    if (!getText.isEmpty()) {
-                        break;
+                // 닉네임
+                tryCount = 1;
+                while (tryCount <= GlobalConstants.tryMaxCount) {
+                    try {
+                        getText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.topp > div.profile > div:nth-child(2) > span.username"))).getText();
+                        if (!getText.isEmpty()) {
+                            break;
+                        }
+                    } catch (Exception e) {
+
                     }
-                } catch (Exception e) {
-
+                    driver.navigate().refresh();
+                    tryCount++;
                 }
-                driver.navigate().refresh();
-                tryCount++;
-            }
-            if (tryCount > GlobalConstants.tryMaxCount) {
-                sb.append("닉네임 tryCount 초과");
-                System.out.println(sb);
-                continue;
-            }
+                if (tryCount > GlobalConstants.tryMaxCount) {
+                    sb.append("닉네임 tryCount 초과");
+                    // System.out.println(sb);
+                    continue;
+                }
 
-            // 갱신 가능 버튼
-            try {
-                element = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.topp > div.profile > div:nth-child(2) > div")));
-                driver.executeScript("arguments[0].click();", element);
-                Thread.sleep(5000);
-            } catch (Exception e) {
-
-            }
-
-            // 현재 티어
-            tryCount = 1;
-            while (tryCount <= GlobalConstants.tryMaxCount) {
+                // 갱신 가능 버튼
                 try {
-                    getText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.table_summary > div:nth-child(2) > div:nth-child(2) > b > font"))).getText();
-                    if (!getText.isEmpty()) {
-                        break;
-                    }
-                } catch (Exception e) {
-
-                }
-                driver.navigate().refresh();
-                tryCount++;
-            }
-            if (tryCount > GlobalConstants.tryMaxCount) {
-                sb.append("현재 티어 tryCount 초과");
-                System.out.println(sb);
-                continue;
-            }
-
-            String tier = getText.split(" ")[0];
-            tier = tier.toLowerCase();
-            tier = Character.toUpperCase(tier.charAt(0)) + tier.substring(1);
-            playerCurSoloRank.setTier(tier);
-
-            if (!(tier.equals("Master") || tier.equals("Grandmaster") || tier.equals("Challenger"))) {
-                String rankNum = getText.split(" ")[1];
-                if (rankNum.equals("I")) {
-                    playerCurSoloRank.setRankNum("1");
-                } else if (rankNum.equals("II")) {
-                    playerCurSoloRank.setRankNum("2");
-                } else if (rankNum.equals("III")) {
-                    playerCurSoloRank.setRankNum("3");
-                } else if (rankNum.equals("IV")) {
-                    playerCurSoloRank.setRankNum("4");
-                }
-            }
-
-            // 솔로 랭크 버튼
-            tryCount = 1;
-            while (tryCount <= GlobalConstants.tryMaxCount) {
-                try {
-                    element = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.rankchamp_S14A_menu > a.sbtn.rankchamp_list.rankchamp_list_solo")));
+                    element = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.topp > div.profile > div:nth-child(2) > div")));
                     driver.executeScript("arguments[0].click();", element);
-                    break;
+                    Thread.sleep(5000);
                 } catch (Exception e) {
 
                 }
-                driver.navigate().refresh();
-                tryCount++;
-            }
-            if (tryCount > GlobalConstants.tryMaxCount) {
-                sb.append("솔로 랭크 버튼 tryCount 초과");
-                System.out.println(sb);
-                continue;
-            }
 
-            // 챔피언 목록
-            tryCount = 1;
-            while (tryCount <= GlobalConstants.tryMaxCount) {
-                try {
-                    wait.until(new ExpectedCondition<Boolean>() {
-                        public Boolean apply(WebDriver driver) {
-                            List<WebElement> elements = driver.findElements(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.rankchamp_S14A_div.rankchamp_S14A_div_solo > table > tbody > tr"));
-                            for (WebElement element : elements) {
-                                if (!element.isDisplayed()) {
-                                    return false;
+                // 현재 티어
+                tryCount = 1;
+                while (tryCount <= GlobalConstants.tryMaxCount) {
+                    try {
+                        getText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.table_summary > div:nth-child(2) > div:nth-child(2) > b > font"))).getText();
+                        if (!getText.isEmpty()) {
+                            break;
+                        }
+                    } catch (Exception e) {
+
+                    }
+                    driver.navigate().refresh();
+                    tryCount++;
+                }
+                if (tryCount > GlobalConstants.tryMaxCount) {
+                    sb.append("현재 티어 tryCount 초과");
+                    // System.out.println(sb);
+                    continue;
+                }
+
+                String tier = getText.split(" ")[0];
+                tier = tier.toLowerCase();
+                tier = Character.toUpperCase(tier.charAt(0)) + tier.substring(1);
+                playerCurSoloRank.setTier(tier);
+
+                if (!(tier.equals("Master") || tier.equals("Grandmaster") || tier.equals("Challenger"))) {
+                    String rankNum = getText.split(" ")[1];
+                    if (rankNum.equals("I")) {
+                        playerCurSoloRank.setRankNum("1");
+                    } else if (rankNum.equals("II")) {
+                        playerCurSoloRank.setRankNum("2");
+                    } else if (rankNum.equals("III")) {
+                        playerCurSoloRank.setRankNum("3");
+                    } else if (rankNum.equals("IV")) {
+                        playerCurSoloRank.setRankNum("4");
+                    }
+                }
+
+                // 솔로 랭크 버튼
+                tryCount = 1;
+                while (tryCount <= GlobalConstants.tryMaxCount) {
+                    try {
+                        element = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.rankchamp_S14A_menu > a.sbtn.rankchamp_list.rankchamp_list_solo")));
+                        driver.executeScript("arguments[0].click();", element);
+                        break;
+                    } catch (Exception e) {
+
+                    }
+                    driver.navigate().refresh();
+                    tryCount++;
+                }
+                if (tryCount > GlobalConstants.tryMaxCount) {
+                    sb.append("솔로 랭크 버튼 tryCount 초과");
+                    // System.out.println(sb);
+                    continue;
+                }
+
+                // 챔피언 목록
+                tryCount = 1;
+                while (tryCount <= GlobalConstants.tryMaxCount) {
+                    try {
+                        wait.until(new ExpectedCondition<Boolean>() {
+                            public Boolean apply(WebDriver driver) {
+                                List<WebElement> elements = driver.findElements(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.rankchamp_S14A_div.rankchamp_S14A_div_solo > table > tbody > tr"));
+                                for (WebElement element : elements) {
+                                    if (!element.isDisplayed()) {
+                                        return false;
+                                    }
+                                }
+                                return true;
+                            }
+                        });
+                        rows = driver.findElements(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.rankchamp_S14A_div.rankchamp_S14A_div_solo > table > tbody > tr"));
+
+                        int mostSeq = 1;
+                        playerCurSoloRank.setMostDatas(new ArrayList<>());
+
+                        for (WebElement row : rows) {
+                            MostData mostData = new MostData();
+                            mostData.setMostSeq(String.valueOf(mostSeq));
+                            mostSeq++;
+
+                            List<WebElement> cols = row.findElements(By.tagName("td"));
+                            for (int i = 0; i < cols.size(); i++) {
+                                String tmp = cols.get(i).getText();
+
+                                if (i == 0) {
+                                    mostData.setChampion(GlobalConstants.championNames.get(tmp));
+                                } else if (i == 1) {
+                                    mostData.setGame(tmp);
+                                } else if (i == 2) {
+                                    double percentage = Double.parseDouble(tmp.substring(0, tmp.length() - 1));
+                                    int win = (int) (Integer.parseInt(mostData.getGame()) * percentage) / 100;
+                                    int lose = Integer.parseInt(mostData.getGame()) - win;
+
+                                    String result = "";
+
+                                    if (win != 0)
+                                        result = result + win + "W";
+                                    if (lose != 0)
+                                        result = result + lose + "L";
+
+                                    result = result + (int) percentage + "%";
+
+                                    mostData.setGame(result);
                                 }
                             }
-                            return true;
+                            playerCurSoloRank.getMostDatas().add(mostData);
                         }
-                    });
-                    rows = driver.findElements(By.cssSelector("#content-container > div:nth-child(1) > div:nth-child(2) > div.rankchamp_S14A_div.rankchamp_S14A_div_solo > table > tbody > tr"));
+                        sb.append("----------------------성공");
+                        // System.out.println(sb);
+                        playerCurSoloRanks.add(playerCurSoloRank);
+                        break;
+                    } catch (Exception e) {
 
-                    int mostSeq = 1;
-                    playerCurSoloRank.setMostDatas(new ArrayList<>());
-
-                    for (WebElement row : rows) {
-                        MostData mostData = new MostData();
-                        mostData.setMostSeq(String.valueOf(mostSeq));
-                        mostSeq++;
-
-                        List<WebElement> cols = row.findElements(By.tagName("td"));
-                        for (int i = 0; i < cols.size(); i++) {
-                            String tmp = cols.get(i).getText();
-
-                            if (i == 0) {
-                                mostData.setChampion(GlobalConstants.championNames.get(tmp));
-                            } else if (i == 1) {
-                                mostData.setGame(tmp);
-                            } else if (i == 2) {
-                                double percentage = Double.parseDouble(tmp.substring(0, tmp.length() - 1));
-                                int win = (int) (Integer.parseInt(mostData.getGame()) * percentage) / 100;
-                                int lose = Integer.parseInt(mostData.getGame()) - win;
-
-                                String result = "";
-
-                                if (win != 0)
-                                    result = result + win + "W";
-                                if (lose != 0)
-                                    result = result + lose + "L";
-
-                                result = result + (int) percentage + "%";
-
-                                mostData.setGame(result);
-                            }
-                        }
-                        playerCurSoloRank.getMostDatas().add(mostData);
                     }
-                    sb.append("----------------------성공");
-                    System.out.println(sb);
-                    playerCurSoloRanks.add(playerCurSoloRank);
-                    break;
-                } catch (Exception e) {
-
+                    tryCount++;
                 }
-                tryCount++;
+                if (tryCount > GlobalConstants.tryMaxCount) {
+                    sb.append("챔피언 목록 tryCount 초과");
+                    // System.out.println(sb);
+                    continue;
+                }
             }
-            if (tryCount > GlobalConstants.tryMaxCount) {
-                sb.append("챔피언 목록 tryCount 초과");
-                System.out.println(sb);
-                continue;
-            }
+        } catch (Exception e) {
+
         }
+
         driver.quit();
         playerCurSoloRankRepository.saveAll(playerCurSoloRanks);
+
     }
 }
