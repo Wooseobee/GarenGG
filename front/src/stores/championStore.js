6a1a3bb4435e8557401038c76e6db51f90b2ebd5
이준범 <1996joon@naver.com>
2024-03-20 15:20:52 +0900

    feat : champion pinia 코드 추가
    Former-commit-id: 85c9ed1f837470acfd3083c76b7e9185df5c7c83

---
+++ front/src/stores/championStore.js
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+import { championInfo } from "@/api/champion.js"
+
+export const useChampionStore = defineStore("champion", {
+  state: () => ({
+    championNames : [],
+    championIds : [],
+    championSquareImgUrls : [],
+  }),
+    actions: {
+        initialize() {
+            //champion id, 이름정보 가져오기
+            championInfo(({data})=>{
+            console.log(data);
+            this.championNames = data.map(item => item.name);
+            this.championIds = data.map(item => item.id);
+            this.championSquareImgUrls = data.map(item => "https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/" + item.id+".png")
+            },
+            (err)=>{
+            console.log(err);
+            });
+        //champion id 기반으로 이미지 url 완성하기
+    },
+}    
+});
