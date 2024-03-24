59cbd84af50f42be141c1e1d964f6606f4537a5d
KimJinyong <wlsdyd4@naver.com>
2024-03-24 22:18:00 +0900

    [Crawling] Fix : time
    Former-commit-id: a8d106caedd6fbcf9b5c60cc59d16553686d7299

--- crawling/apiKeyCrawling/src/main/java/org/example/apikeycrawling/global/MyStartupTask.java
+++ crawling/apiKeyCrawling/src/main/java/org/example/apikeycrawling/global/MyStartupTask.java
@@ -66,37 +66,40 @@ public class MyStartupTask implements ApplicationRunner {
         Thread.sleep(60000);
 
 
+        int count = 0;
         while (true) {
 
             String uuid = UUID.randomUUID().toString();
 
             driver.get("https://developer.riotgames.com/app-type");
-            Thread.sleep(3000);
+            Thread.sleep(2000);
 
             webElement = driver.findElement(By.xpath("/html/body/div[2]/div/div[4]/div/div[2]/div/button"));
             driver.executeScript("arguments[0].click();", webElement);
-            Thread.sleep(3000);
+            Thread.sleep(2000);
 
             webElement = driver.findElement(By.xpath("/html/body/div[2]/div/div[5]/div/div/div[3]/a[1]"));
             driver.executeScript("arguments[0].click();", webElement);
-            Thread.sleep(3000);
+            Thread.sleep(2000);
 
             driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/form/div[1]/div[1]/div/input")).sendKeys(uuid);
             driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/form/div[1]/div[2]/div/textarea")).sendKeys(uuid);
-            Thread.sleep(3000);
+            Thread.sleep(2000);
 
             webElement = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/form/div[3]/input"));
             driver.executeScript("arguments[0].click();", webElement);
-            Thread.sleep(3000);
+            Thread.sleep(2000);
 
             String key = driver.findElement(By.xpath("/html/body/div[2]/div/form/div/div/div[5]/ul/li[6]/span")).getText();
-            Thread.sleep(3000);
+            Thread.sleep(2000);
 
             ApiKey apiKey = new ApiKey();
             apiKey.setApiKey(key);
             apiKey.setUuid(uuid);
 
             apiKeyRepository.save(apiKey);
+            count++;
+            System.out.println("count = " + count);
         }
     }
 
@@ -111,7 +114,7 @@ public class MyStartupTask implements ApplicationRunner {
 
                 RestTemplate restTemplate = new RestTemplate();
                 String url = "https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}?api_key={apiKey}";
-                AccountDto accountDto = restTemplate.getForObject(url, AccountDto.class, "틀딱기", "KR1", apiKeys.get(0).getApiKey());
+                AccountDto accountDto = restTemplate.getForObject(url, AccountDto.class, "틀딱기", "KR1", apiKeys.get(i).getApiKey());
                 System.out.println("count = " + count + " i = " + i + " " + accountDto.getPuuid());
 
             }
