03b5db02d52149e6cac45d173ac58c0a5da9e7fb
wooseobee <dntjq0807@naver.com>
2024-03-25 16:43:52 +0900

    [BE] Fix : requestMapping url 수정
    Former-commit-id: dc2d7336e5cbc93b33ed07626589fbad8f1671f4

--- back/src/main/java/gg/garen/back/matchPrediction/controller/MatchPredictionController.java
+++ back/src/main/java/gg/garen/back/matchPrediction/controller/MatchPredictionController.java
@@ -3,11 +3,15 @@ package gg.garen.back.matchPrediction.controller;
 import gg.garen.back.matchPrediction.dto.RandomMatchResponseDto;
 import gg.garen.back.matchPrediction.service.MatchPredictionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class MatchPredictionController {
 
     private final MatchPredictionService matchPredictionService;
