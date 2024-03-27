922b5bdb6a0dd0f7d56a3f27af1937dd158696b6
wooseobee <dntjq0807@naver.com>
2024-03-27 12:14:24 +0900

    [BE] Fix : ranking data를 내림차순 정렬하도록 수정
    Former-commit-id: a6843f53f1a957f9a624162ca38f8ec377417f7a

--- back/src/main/java/gg/garen/back/common/repository/RankingRepository.java
+++ back/src/main/java/gg/garen/back/common/repository/RankingRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RankingRepository extends JpaRepository<Ranking, Long> {
-    List<Ranking> findTop10ByGameId(Long gameId);
+    List<Ranking> findTop10ByGameIdOrderByScoreDesc(Long gameId);
 }
