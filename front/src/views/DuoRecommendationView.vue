1c6eaa47ff9dec4fdc2a3ad1bbabb2ab19b41e94
이준범 <1996joon@naver.com>
2024-03-28 15:33:55 +0900

    feat: DuoRecommendationView에 있는내용 DuoRecommendationSearch로 옮김
    Former-commit-id: 6a58728d96dd7932208a73ff57027e9b0103ea32

--- front/src/views/DuoRecommendationView.vue
+++ front/src/views/DuoRecommendationView.vue
@@ -1,24 +1,12 @@
 <template>
   <div>
-    <transition name="fade">
-    <div v-if="showIntro" class="intro-container section-title">
-      <!-- 로고 이미지 또는 텍스트 -->
-      <!-- <img src="@/assets/your-logo.png" alt="듀오챔피언추천 로고"> -->
-      <h1> 듀ㅡ챔ㅡ추 </h1>
-    </div>
-  </transition>
-    <transition name="fade">
-      <div class="content" v-if="!showIntro">
+      <div class="content" >
         <!-- 기존 내용 -->
         <Header />
-        <div class="container section-title" data-aos="fade-up">
-          <h1>듀오 챔피언 추천</h1>
           <div>
             <RouterView></RouterView>
           </div>
-        </div>
       </div>
-    </transition>
   </div>
 </template>
 <script setup>
@@ -26,17 +14,9 @@ import { ref } from 'vue';
 import Header from "@/components/common/Header.vue";
 import router from '@/router';
 
-const showIntro = ref(true);
-
-setTimeout(() => {
-  showIntro.value = false;
-  router.push({
-    name : "DuoRecommendationSearch"
-  });
-}, 3000); // 3초 후 로고 숨김
 </script>
 
-<style scoped>
+<style >
 .section-title {
   text-align: center;
   padding-bottom: 60px;
@@ -75,7 +55,7 @@ setTimeout(() => {
 
 /* intro setting */
 .fade-enter-active, .fade-leave-active {
-  transition: opacity 2s ease;
+  transition: opacity 1.0s ease;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
   opacity: 0;
@@ -86,6 +66,7 @@ setTimeout(() => {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column; /* 요소들을 수직 방향으로 정렬 */
   height: 100vh; /* 화면 전체 높이 */
 }
 
