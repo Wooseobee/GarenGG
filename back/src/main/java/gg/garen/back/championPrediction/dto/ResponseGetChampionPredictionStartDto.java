e01d1e1ced84b31a1ce496e92fec967eea1538d1
KimJinyong <wlsdyd4@naver.com>
2024-03-29 15:52:09 +0900

    [BE] Feat : sound champion game
    Former-commit-id: f8d02b7687196cf22a4d9d7f977b311a99772817

---
+++ back/src/main/java/gg/garen/back/championPrediction/dto/ResponseGetChampionPredictionStartDto.java
@@ -0,0 +1,29 @@
+package gg.garen.back.championPrediction.dto;
+
+import gg.garen.back.champion.dto.ChampionDto;
+import gg.garen.back.champion.entity.Champion;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResponseGetChampionPredictionStartDto {
+
+    private List<Round> rounds;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Round {
+        private ChampionDto answerChampion;
+        private List<ChampionDto> candidateChampions;
+    }
+
+
+}
