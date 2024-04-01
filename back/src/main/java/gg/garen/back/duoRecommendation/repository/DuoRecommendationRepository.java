dc5b4434e6d8dd69df5d7f698375890fd7bc0e73
이준범 <1996joon@naver.com>
2024-04-01 11:06:16 +0900

    feat : 듀오챔피언 판수10판이상 승률50이상 승리수 기준 정렬
    Former-commit-id: d6a8b2d48e5ab1ba2590302bb47ba9b038fed3f9

--- back/src/main/java/gg/garen/back/duoRecommendation/repository/DuoRecommendationRepository.java
+++ back/src/main/java/gg/garen/back/duoRecommendation/repository/DuoRecommendationRepository.java
@@ -14,7 +14,7 @@ public interface DuoRecommendationRepository extends JpaRepository<DuoRecord, In
     List<Object[]> findAllNamesAndKeysAndIds();
     @Query(value = "SELECT * " +
             "FROM duo_record d " +
-            "where ((d.champion1 = :championKey AND d.lane1 = :position )  OR (d.champion2 = :championKey AND d.lane2 = :position)) AND d.total_match >= 5 AND d.win_rate >= 50 " +
-            "order by d.win_rate", nativeQuery = true)
+            "where ((d.champion1 = :championKey AND d.lane1 = :position )  OR (d.champion2 = :championKey AND d.lane2 = :position)) AND d.total_match >= 10 AND d.win_rate >= 50 " +
+            "order by victory desc, d.win_rate desc", nativeQuery = true)
     List<DuoRecord> findDuoRecordByChampionName(@Param("championKey") Long championKey, @Param("position") String position);
 }
